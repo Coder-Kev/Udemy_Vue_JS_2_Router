69d61b5f7332ea7aea83fea927542dba48bba58d
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2018-11-11 19:42:22 -0800

    Sect04-Lect31: Watching the $route Object

---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile for {{ userId }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewProfile",
+  data() {
+    return {
+      userId: this.$route.params.user_id
+    };
+  },
+  methods: {
+    updateId() {
+      // force update when moving from one Profile ID to another
+      this.userId = this.$route.params.user_id;
+    }
+  },
+  watch: {
+    $route: "updateId"
+  }
+};
+</script>
+
+<style></style>
