b4799d9684eeb7f7ac3f6c05c3beaf431ccd53ab
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2018-11-11 20:24:01 -0800

    Sect04-Lect33:  Programmatically Redirecting Users

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,20 @@
       <li><router-link to="/">Home</router-link></li>
       <li><router-link :to="{ name: 'About' }">About</router-link></li>
     </ul>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <h2>Navigation Controls</h2>
+    <ul>
+      <li><button @click="goBack">Go Back</button></li>
+      <li><button @click="goHome">Redirect To Home</button></li>
+      <li><button @click="goForward">Go Forward</button></li>
+    </ul>
   </nav>
 </template>
 
@@ -11,7 +25,26 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods: {
+    goHome() {
+      // $router keeps track of our Navigation History
+      // same as response.redirect
+      this.$router.push({ name: "Home" });
+    },
+    goBack() {
+      console.log("On codesandbox.io goBack changes URL but does not render");
+      this.$router.go(-1);
+    },
+    goForward() {
+      console.log(
+        "On codesandbox.io goForward changes URL but does not render"
+      );
+      this.$router.go(1);
+    }
   }
 };
 </script>
@@ -19,10 +52,14 @@ export default {
 // NOTE that router-link become anchor tags
 <style>
 ul {
-  lit-style-type: none;
+  list-style-type: none;
   padding: 0;
 }
 a {
   color: #42b983;
+}
+li {
+  display: inline-block;
+  margin: 10px;
 }
 </style>
